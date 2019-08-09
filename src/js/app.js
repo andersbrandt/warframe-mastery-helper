76350014bd4e49fc0a064e4d448ea48886a9a7b6
Anders Brandt <anders.brandt@me.com>
2019-08-09 15:33:01 +0200

    Upped version

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,7 @@ const ClockOrbvallis = require("./clock-orbvallis.js");
 
 var app = {
   name: "Warframe Mastery Helper",
-  version: "2.4.4",
+  version: "2.4.5",
   data: {},
   config: {
     modal: {
