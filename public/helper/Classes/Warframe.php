79a57f11ed1657d6935fec33c9abd0c555684741
Anders Brandt <anders.brandt@me.com>
2020-03-07 08:36:48 +0100

    Refactored savedata

--- public/helper/Classes/Warframe.php
+++ public/helper/Classes/Warframe.php
@@ -8,42 +8,64 @@ class Warframe
     public function saveData($userId, $data)
     {
         global $db;
-        global $config;
         global $functions;
         $db->connect();
         $data = $functions->returnSafe(serialize($data));
         $data = base64_encode($data);
         $date = date("Y-m-d H:i:s");
-        // Find the users entry, update OR insert
-        $db->select($this->dataTbl, "id", null, "userId = '$userId'", "id desc", "1");
-        $userEntry = $db->getResult();
-        //TODO check for errors!
-        if (count($userEntry)) {
-            // UPDATE
-            $userEntryId = $userEntry[0]["id"];
-            $db->update($this->dataTbl, array(
-                "data" => $data,
-                "timeStamp" => $date,
-                "userId" => $userId
-                ), "id = '$userEntryId'");
-            $db->getResult();
+        $userEntryId = $this->getUserEntryId($userId);
+        if ($userEntryId) {
+            $this->updateData($data, $date, $userId, $userEntryId);
             return 1;
         } else {
-            // INSERT
-            $db->insert($this->dataTbl, array(
-                "data" => $data,
-                "timeStamp" => $date,
-                "userId" => $userId
-            ));
-            $result = $db->getResult();
+            $result = $this->insertData($data, $date, $userId);
             return $result;
         }
+    }
+
+    private function getUserEntryId($userId) {
+        global $db;
+        $db->connect();
+        $db->select($this->dataTbl, "id", null, "userId = '$userId'", "id desc", "1");
+        $userEntry = $db->getResult();
+        if (count($userEntry)) {
+            if(isset($userEntry[0]["id"])){
+                return $userEntry[0]["id"];
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    private function updateData($data, $date, $userId, $userEntryId) {
+        global $db;
+        $db->connect();
+        $db->update($this->dataTbl, array(
+            "data" => $data,
+            "timeStamp" => $date,
+            "userId" => $userId
+            ), "id = '$userEntryId'");
+        $db->getResult();
+        return 1;
+    }
+
+    private function insertData($data, $date, $userId) {
+        global $db;
+        $db->connect();
+        $db->insert($this->dataTbl, array(
+            "data" => $data,
+            "timeStamp" => $date,
+            "userId" => $userId
+        ));
+        $result = $db->getResult();
+        return $result;
     }
 
     public function getData($userId)
     {
         global $db;
-        global $config;
         global $functions;
         $db->connect();
         $userId = $functions->returnSafe($userId);
